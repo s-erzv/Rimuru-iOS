ab5e5f09da33e5e869708bb157469336c6b9a42c
s-erzv <sarahfajriarahmah@gmail.com>
2026-03-11 17:55:20 +0700

    fix config

--- Rimuru/Core/Config.swift
+++ Rimuru/Core/Config.swift
@@ -1,15 +1,12 @@
-//
-//  Config.swift
-//  Rimuru
-//
-//  Created by MACBOOK AIR on 05/03/26.
-//
-
-
 import Foundation
 
 enum Config {
-    // Taruh API Key lo di sini. 
-    // Untuk production, biasanya ini ditaruh di .xcconfig file biar ga masuk git.
-    static let geminiAPIKey = "AIzaSyD2rtZIC1lzKDKIp4BcgFYyL8yQMm2YpcA"
+    static let geminiAPIKey: String = {
+        guard let filePath = Bundle.main.path(forResource: "Info", ofType: "plist"),
+              let plist = NSDictionary(contentsOfFile: filePath),
+              let value = plist["GEMINI_API_KEY"] as? String else {
+            fatalError("Couldn't find key 'GEMINI_API_KEY' in Info.plist.")
+        }
+        return value
+    }()
 }
